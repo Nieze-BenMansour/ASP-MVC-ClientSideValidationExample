db6b4fc86d1e48bc8996a76b814dd94809ea4924
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-29 10:54:27 +0100

    Add project files.

---
+++ AsynchronousValidationExample/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsynchronousValidationExample.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Le nom est requis.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "L'adresse e-mail est requise.")]
+        [EmailAddress(ErrorMessage = "Adresse e-mail invalide.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est requis.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères.")]
+        public string Password { get; set; }
+    }
+}
